9d82d1a0106d53588633a592006d36ce2a14d65f
BeijingMa <beijingm@vmware.com>
2022-05-12 13:06:27 +0800

    增加celery的状态传递说明。

--- tasks/background.py
+++ tasks/background.py
@@ -3,17 +3,21 @@ import time
 from celery import current_app as celery
 
 from models import *
-from . import celery_logger
 
 
 @celery.task(name='tasks.background.async_get_address', bind=True, serializer='json')
 def async_get_address(self, user_id):
+    """
+    通过给task(bind=True)，让被装饰task第一个参数接收self，就是task本身。
+    通过指定serializer序列化返回值
+    self的自身状态有几种。PROGRESS， INIT, FAIL, SUCCESS，其中PROGRESS就是执行中，可以插入meta=dict来增加进度，状态之类。
+    通过celery.AsyncResult.result就可以拿到执行结果，或者status.meta的属性。
+    """
     self.update_state(state="PROGRESS", meta={'progress': 10})
-    celery_logger.error('this is error log.')
-    time.sleep(10)
+    time.sleep(5)
     self.update_state(state="PROGRESS", meta={'progress': 50})
     user = User.query.filter_by(user_id=user_id).first()
     address_list = user.address.filter_by(user_id=user_id).all()
-    time.sleep(10)
+    time.sleep(5)
     self.update_state(state="PROGRESS", meta={'progress': 100})
     return [address.serialize() for address in address_list]
